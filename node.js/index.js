9bc286307c5e44d01ad86d50da01b944b7729354
Chloe <chloe.martins@etec.sp.gov.br>
2024-10-10 10:07:45 -0300

    Revert unlimited framerate

--- node.js/index.js
+++ node.js/index.js
@@ -46,6 +46,15 @@ function badApple() {
         console.time('Animation Execution Time');
         if (!isRunning) return;
 
+        /**
+        * On previous versions, I used to clear the screen every time a new frame was played.
+        * Setting the cursor position to the top left instead overwrites every frame with the new one
+        * and is a major performance improvement, as clearing the screen is an expensive operation.
+        * 
+        * For comparison, the animation, when running in unlimited fps finished at 50.3 seconds
+        * using the screen-clearing version, and 0.8 seconds when setting the cursor position instead.
+        */
+        //process.stdout.write('\x1Bc');
         setCursorPosition(0,0);
 
         try {
@@ -57,8 +66,8 @@ function badApple() {
 
         curFrame++;
         if (curFrame < maxFrame && isRunning) {
-            //setTimeout(playAnimation, 33); // Schedule next frame
-            playAnimation();
+            setTimeout(playAnimation, 33); // Schedule next frame
+            //playAnimation(); //For performance testing purposes
         } else {
             console.timeEnd('Animation Execution Time');
             process.exit(); // Exit when animation completes or if isRunning is false
